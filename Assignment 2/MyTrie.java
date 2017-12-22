c4ace41bd1288e829cfbd6438318d5a99838eb32
Omar-EB <32253541+Omar-EB@users.noreply.github.com>
2017-12-21 23:47:03 -0500

    Update MyTrie.java

--- Assignment 2/MyTrie.java
+++ Assignment 2/MyTrie.java
@@ -1,12 +1,6 @@
 /*** 
  * This is class implements a trie that holds a set of strings.
  * MyTrie stores stores nodes using class TreeNode
- * 
- * Name:   
- * Student Number: 
- * Uottawa Email: 
- * 
- *
  */
 
 public class MyTrie {
@@ -15,13 +9,12 @@ public class MyTrie {
 
 	private int numNodes;
 
-    // Constructor. Note that an empty trie (no strings added) contains the root node 
+    // Constructor. An empty trie (no strings added) contains the root node 
 	public MyTrie() {
 		root = new TreeNode(null, null, null,false); 
 		numNodes=1;
 	}
 
-	// Method to be implemented by you. See handout part 1A
 	public boolean insert(String s) {
 	
 		if (s==null){
@@ -91,14 +84,10 @@ public class MyTrie {
 		}
 		
 	
-		// ***** method code to be added in this class *****
-		// now we just have a dummy that prints  message and returns true.
-		//System.out.println("insert(String) not implemented!");
 		return true; //sentinential return value
 
 	}
 	
-	// Method to be implemented by you. See handout part 1A
 	public boolean search(String s) {
 		
 		if (s==null){
@@ -148,23 +137,12 @@ public class MyTrie {
 				}
 			}
 		}
-		
-		
-		
-		// **** method code to be added in this class *****
-		// now we just have a dummy that prints  message and returns true.
-		//System.out.println("search(String) not implemented!");
-		return false;	
-	    
+		return false;
 	}
 
 	
 
-	// Method to be implemented by you. See handout part 1A	
 	public void printStringsInLexicoOrder() {
-		// ***** method code to be added in this class *****
-		// now we just have a dummy method that prints a message.
-		//System.out.println("printStringsInLexicoOrder() not implemented!");
 		printStringsInLexicoOrder(root,"") ;
 	}
 	
@@ -181,10 +159,12 @@ public class MyTrie {
 	}
 	
 	
-	public void printInOrder() { // not to be changed
+	public void printInOrder() {
 		printInOrder(root);
 	}
-	private void printInOrder(TreeNode N) { // not to be changed
+	
+	//recursive part 
+	private void printInOrder(TreeNode N) {
 		System.out.print("(");
 		if (N!=null) {
 			printInOrder(N.getLeftChild());
@@ -197,11 +177,11 @@ public class MyTrie {
 	
 
 	
-	public TreeNode root() { // not to be changed
+	public TreeNode root() {
 		return root;
 	}
 	
-	public int numNodes() { // not to be changed
+	public int numNodes() {
 		return numNodes;
 	}
 
